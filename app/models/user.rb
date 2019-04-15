17693e7e9c6bd5123bfceba310724bf160df638e
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-15 22:30:35 +0700

    try remember digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_secure_password
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true, confirmation: true
 
-  before_create do
-    token = SecureRandom.urlsafe_base64
-    Digest::SHA1.hexdigest(token.to_s)
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
