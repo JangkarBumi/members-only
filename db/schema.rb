aa4937e322237610cc320c80a98f5ae446a7ff55
1ba1 <vvvalenti01@yahoo.it>
2019-04-15 15:31:34 +0200

    basic setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_130500) do
+ActiveRecord::Schema.define(version: 2019_04_15_131828) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_130500) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_confirmation"
   end
 
 end
